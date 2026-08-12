d6f13675ad8d609379a8eef51869228cb7223bb0
DEV-BR0 <lazizbekxoljigitov@gmail.com>
2026-08-12 22:43:12 +0500

    sanr

--- src/hooks/axios.js
+++ src/hooks/axios.js
@@ -1,5 +1,5 @@
-import axios from "axios";
+import axios from 'axios'
 export const api = axios.create({
-  baseURL: "https://otaku-hub-pink.vercel.app/",
+  baseURL: 'https://6a607c54b1933e9d25fd54f0.mockapi.io/1v/:users',
   timeout: 2000,
-});
+})
